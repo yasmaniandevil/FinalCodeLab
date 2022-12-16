f3cda136d07e97c90aa9ca94c6d43dd4be0b0943
yasmaniandevil <44387512+yasmaniandevil@users.noreply.github.com>
2022-12-16 00:12:27 -0500

    Destroy

---
+++ CodeLabFinal/Assets/Scripts/ObjMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjMovement : MonoBehaviour
+{
+
+    public GameObject objMove;
+    public float speed = 2;
+    public GameObject obstacle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newPos = transform.position;
+        //creating vector 2 called new pos, thats where it currently is
+
+        newPos.x += speed;
+        //x position plus how fast 
+
+        transform.position = newPos;
+        //transform the actual position
+
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            Destroy(obstacle.gameObject);
+            Destroy(objMove.gameObject);
+        }
+     
+    }
+}
